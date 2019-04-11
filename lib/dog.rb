9039f262593d61ba1352e9ed09c49f6ab627a595
Robert Tate <btate712@gmail.com>
2019-04-11 08:24:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,10 @@
 class Dog
   attr_accessor :name, :breed, :id
 
-  def initialize(name:, breed:, :id)
+  def initialize(name:, breed:, id: = nil)
     @name = name
     @breed = breed
     @id = id
-  end
-
-  def self.attributes(parms)
-
-
   end
 
   def self.create_table
