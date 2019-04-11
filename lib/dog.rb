d4e200595f030eb7617a6fc16421308e4d5ec33e
Robert Tate <btate712@gmail.com>
2019-04-11 08:15:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,9 +4,9 @@ class Dog
   def initialize(name, breed, id = nil)
     @name = name
     @breed = breed
-    @id = id 
+    @id = id
   end
-  
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE dogs (
