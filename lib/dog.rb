6981818d740c76fe30ab6b10a7177dcef2bd14a6
Robert Tate <btate712@gmail.com>
2019-04-11 08:15:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,12 @@
 class Dog
   attr_accessor :name, :breed, :id
 
+  def initialize(name, breed, id = nil)
+    @name = name
+    @breed = breed
+    @id = id 
+  end
+  
   def self.create_table
     sql = <<-SQL
       CREATE TABLE dogs (
@@ -9,7 +15,6 @@ class Dog
         breed TEXT
       )
     SQL
-
     DB[:conn].execute(sql)
   end
 end
